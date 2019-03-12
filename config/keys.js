07373003e8f696baee9a2f40e971109314c620d5
root <root@DESKTOP-P0JDALQ.localdomain>
2019-03-12 14:44:53 -0500

    initial express server with routes

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb://rhigg:326932@localhost:27017/admin'
+  };
+  
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express()
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+//Db Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose 
+    .connect(db, {useNewUrlParser : true})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+//Use Routes 
+app.use('/api/users', users)
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
+
+const port = process.env.PORT || 5000 
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`))

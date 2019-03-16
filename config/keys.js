c18a17e1bc39b998628e43d3e09516a95dc7a952
rhigg <rhiggins32@gmail.com>
2019-03-15 20:50:40 -0500

    passport authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://rhigg:3232@localhost:27017/devconnector?authSource=admin'
+    mongoURI: 'mongodb://rhigg:3232@localhost:27017/devconnector?authSource=admin',
+    secretOrKey: 'secret'
   };
   

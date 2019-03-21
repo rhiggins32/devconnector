7c422608d9a0914e104c8233b1001b1d8d6deef6
rhigg <rhiggins32@gmail.com>
2019-03-20 19:20:49 -0500

    show profile and show all profiles routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,7 +21,7 @@ router.get(
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
-      .populate('user',['name', 'avatar'])
+      .populate("user", ["name", "avatar"])
       .then(profile => {
         if (!profile) {
           errors.noprofile = "There is no profile for this user";
@@ -32,6 +32,58 @@ router.get(
       .catch(err => res.status(404).json(err));
   }
 );
+
+//@route GET api/profile/handle/:handle
+//@desc Get profile by handle
+//@access Public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user : req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route GET api/profile/user/:user_id
+//@desc Get profile by user ID
+//@access Public
+
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+//@route GET api/profile/all
+//@desc Get all profiles
+//@access public
+router.get('/all', (req, res) => {
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if(!profiles){
+        error.noprofiles = "There are no profiles";
+        return res.status(404).json(errors)
+      }
+      res.json(profiles)
+    })
+      .catch(err => res.status(404).json({profile: 'There are no profiles'}));
+});
 
 //@route POST api/profile
 //@desc Create or Edit user profile
@@ -64,8 +116,7 @@ router.post(
       social: { youtube, twitter, facebook, linkedin, instagram }
     };
 
-    Profile.findOne({ user: req.user.id })
-    .then(profile => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
       if (profile) {
         //Update
         Profile.findOneAndUpdate(

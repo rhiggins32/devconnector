07e03a5ea6b0703ae41a5657d311b53e010ecb3b
root <root@DESKTOP-P0JDALQ.localdomain>
2019-04-01 12:58:53 -0500

    build folder and heroku details

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const path = require('path');
 
 const app = express()
 
@@ -10,6 +10,13 @@ const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+    }));
+}
 //Body Parser Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());

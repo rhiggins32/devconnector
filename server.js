b7dcd1d8d39f53623044b0a2d1919d07455ab5f8
root <root@DESKTOP-P0JDALQ.localdomain>
2019-04-01 13:22:14 -0500

    heroku

--- server.js
+++ server.js
@@ -1,46 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 //Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
-    }));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get(
+    "*",
+    (req,
+    res => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+  );
 }
 //Body Parser Middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Db Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
-mongoose 
-    .connect(db, {useNewUrlParser : true})
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 //Passport Middleware
 app.use(passport.initialize());
 
 //Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-//Use Routes 
-app.use('/api/users', users)
-app.use('/api/posts', posts)
-app.use('/api/profile', profile)
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
